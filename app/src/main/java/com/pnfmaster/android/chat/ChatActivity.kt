5581db480db8e8013f745c318624e116116b94a7
nayufeng <yufengna820@gmail.com>
2024-04-17 17:14:11 +0800

    1. 在TasksActivity中插入人工智能API。 2. 目前的问题：当ai返回的答案较长时可能出现ui被截断的情况。

--- app/src/main/java/com/pnfmaster/android/chat/ChatActivity.kt
+++ app/src/main/java/com/pnfmaster/android/chat/ChatActivity.kt
@@ -1,19 +1,17 @@
 package com.pnfmaster.android.chat
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.pnfmaster.android.R
 import com.pnfmaster.android.databinding.ActivityChatBinding
-import org.json.JSONException
-import java.io.IOException
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var binding : ActivityChatBinding
@@ -23,18 +21,17 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Initialize Interface
         binding = ActivityChatBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
         rc_chatlist = binding.rcChatlist
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setSupportActionBar(binding.toolbarChat)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.title = getString(R.string.app_name)
         }
 
         // Initialize Greeting Message
         val mData = ArrayList<Chatlist>()
-        val firstChat = Chatlist("PMaster: ", "你好！我是人工智能助理，请问你有什么需要帮助的呢？")
+        val firstChat = Chatlist("PNFMaster: ", "你好！我是PNF Master，请问你有什么需要帮助的呢？")
         mData.add(firstChat)
 
         // Set adapter and manager for recyclerView
@@ -52,33 +49,48 @@ class ChatActivity : AppCompatActivity() {
             // Update data
             chatAdapter.update(mData)
             rc_chatlist.adapter = chatAdapter
+            // Clear EditText
+            binding.etChat.setText("")
 
             // Get answer from AI
             Thread {
-                var reply: Chatlist
-                try {
+                val reply = try {
                     val ai = AIAssistant()
-                    reply = Chatlist("PNFMaster: ", ai.GetAnswer(user_ask))
+                    Chatlist("PNFMaster: ", ai.GetAnswer(user_ask))
                 } catch (e: Exception) {
-                    reply = Chatlist("PNFMaster: ", "Error")
+                    Log.e("ChatActivity", e.toString())
+                    Chatlist("PNFMaster: ", "Error")
                 }
                 mData.add(reply)
-                chatAdapter.update(mData)
 
                 // 子线程中不能直接更新UI，必须发给handler在主线程中完成
                 val msg = Message()
                 msg.what = MESSAGE_UPDATE
+                msg.obj = mData
                 myHandler().sendMessage(msg)
             }.start()
         }
     } // onCrete
 
+    @SuppressLint("HandlerLeak")
     inner class myHandler : Handler(Looper.getMainLooper()) {
+        @Suppress("UNCHECKED_CAST")
         override fun handleMessage(msg: Message) {
             when (msg.what) {
-                MESSAGE_UPDATE ->  rc_chatlist.adapter = chatAdapter
+                MESSAGE_UPDATE ->  {
+                    val mData = msg.obj as List<Chatlist>
+                    chatAdapter.update(mData)
+                    rc_chatlist.adapter = chatAdapter
+                }
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressedDispatcher.onBackPressed()
+        }
+        return true
     }
 
     companion object {
