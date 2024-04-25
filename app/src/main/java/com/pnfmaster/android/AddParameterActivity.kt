fcf90a6c4552b68a3a7789dcb6d0f699e92adae3
nayufeng <yufengna820@gmail.com>
2024-04-25 19:12:40 +0800

    UI change. 1. New AddParameterActivity 2. Beautify other UIs.

---
+++ app/src/main/java/com/pnfmaster/android/AddParameterActivity.kt
@@ -0,0 +1,58 @@
+package com.pnfmaster.android
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.pnfmaster.android.databinding.ActivityAddParameterBinding
+import kotlin.random.Random
+
+class AddParameterActivity : BaseActivity() {
+    private lateinit var binding : ActivityAddParameterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddParameterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.title = getString(R.string.my_param)
+        }
+
+        binding.generateParameters.setOnClickListener {
+            // Generate random parameters
+            val upperLimit = Random.nextInt(100, 200)
+            val lowerLimit = Random.nextInt(0, 100)
+            val motorPosition = Random.nextInt(0, 100)
+            val trainingTime = Random.nextInt(1, 60)
+
+            // Update EditTexts
+            binding.forceUpperLimit.setText(upperLimit.toString())
+            binding.forceLowerLimit.setText(lowerLimit.toString())
+            binding.motorPosition.setText(motorPosition.toString())
+            binding.trainingTime.setText(trainingTime.toString())
+
+            // Update chart data
+            val entries = ArrayList<Entry>()
+            entries.add(Entry(0f, lowerLimit.toFloat()))
+            entries.add(Entry(1f, upperLimit.toFloat()))
+
+            val dataSet = LineDataSet(entries, "Force")
+            val data = LineData(dataSet)
+            binding.chart.data = data
+            binding.chart.invalidate() // refresh chart
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressedDispatcher.onBackPressed()
+        }
+        return true
+    }
+}
